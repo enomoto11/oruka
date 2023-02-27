8eb63a647685da3e151542f454407eb61070df24
enomoto11 <t.enomoto@catallaxy>
2023-02-27 17:17:01 +0900

    DB接続不良を解消（parse,closing）

--- server/src/db-connection/connection.go
+++ server/src/db-connection/connection.go
@@ -11,15 +11,15 @@ import (
 )
 
 func Connect() *ent.Client {
-	path := fmt.Sprintf("%s:%s@tcp(db:3306)/%s", os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_DATABASE"))
+	path := fmt.Sprintf("%s:%s@tcp(db:3306)/%s?charset=utf8&parseTime=true", os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_DATABASE"))
 
-	client, err := ent.Open("mysql", path)
+	client, clientErr := ent.Open("mysql", path)
 
-	if err != nil {
-		log.Fatalf("failed connect to mysql: %v", err)
+	if clientErr != nil {
+		log.Fatalf("failed connect to mysql: %v", clientErr)
 	}
 
-	defer client.Close()
+	// defer client.Close()
 
 	ctx := context.Background()
 
